0f4dd4923812fd703a698c0fed6ff26ab42b5fb2
romulolink <romulolink@gmail.com>
2022-06-07 18:51:42 -0300

    Versao inicial

---
+++ Scenes/Move.gd
@@ -0,0 +1,18 @@
+extends MeshInstance
+
+
+# Declare member variables here. Examples:
+var position_step = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	position_step+=1
+	transform.origin.y = position_step
+	
